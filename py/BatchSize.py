de887824b96d3aa5a20cc80448b4ca280678bd97
css <cs90911@gmail.com>
2022-06-11 22:13:32 +0800

    chkpnt: draw batch size

---
+++ py/BatchSize.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main(argv):
+    argv[1]
+    names = ["LNet0", "LNet1", "LNet*"] #, "Airtel1", "Stanford", "Internet2"]
+    
+    for name in names:
+        f = open(argv[1] + "/" + name + "bPuUs.txt", "r")
+        
+        line = f.readline()
+        totalSize = int(line)
+
+        x, y = [], []
+        while True:
+            line = f.readline()
+            if not line:
+                break;
+
+            f_list = [float(i) for i in line.split(" ") if i.strip()]
+            x.append(f_list[0] / totalSize)
+            y.append(f_list[1])
+
+        plt.plot(x, y, label=name)
+
+    plt.xlabel('Batch size / total #updates')
+    plt.ylabel('Model update time (s)')
+    plt.yscale('log')
+    plt.title("Model update time for different block size")
+    plt.show()
+
+if __name__ == "__main__":
+    main(sys.argv)
